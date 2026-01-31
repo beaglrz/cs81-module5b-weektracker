d67b87ee25c6e1a8d686cd6da0342c67bd515c81
Beatriz Galarza <beatriz.glrz@gmail.com>
2026-01-31 01:45:58 -0800

    Update weekly activity data

---
+++ activityTracker.js
@@ -0,0 +1,63 @@
+/*
+Beatriz Galarza
+Repository: https://github.com/beaglrz/cs81-module5b-weektracker
+*/
+
+const myWeek = [
+  {
+    day: "Monday",
+    activity: "In-person class on campus",
+    category: "creative",
+    hoursSpent: 2,
+    enjoyment: 8,
+    timeOfDay: "afternoon"
+  },
+  {
+    day: "Tuesday",
+    activity: "Database homework",
+    category: "creative",
+    hoursSpent: 2,
+    enjoyment: 7,
+    timeOfDay: "evening"
+  },
+  {
+    day: "Wednesday",
+    activity: "Presented a speech in class",
+    category: "social",
+    hoursSpent: 2,
+    enjoyment: 6,
+    timeOfDay: "morning"
+  },
+  {
+    day: "Thursday",
+    activity: "Cook dinner",
+    category: "creative",
+    hoursSpent: 1.0,
+    enjoyment: 9,
+    timeOfDay: "evening"
+  },
+  {
+    day: "Friday",
+    activity: "Completed JavaScript assignments",
+    category: "creative",
+    hoursSpent: 5,
+    enjoyment: 7,
+    timeOfDay: "evening"
+  },
+  {
+    day: "Saturday",
+    activity: "Gym workout",
+    category: "physical",
+    hoursSpent: 1,
+    enjoyment: 9,
+    timeOfDay: "morning"
+  },
+  {
+    day: "Sunday",
+    activity: "Caught up on my TV shows",
+    category: "creative",
+    hoursSpent: 3,
+    enjoyment: 9,
+    timeOfDay: "evening"
+  }
+];
